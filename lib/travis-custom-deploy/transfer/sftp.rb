9d0c80317f2dcd989a770bbb50069a6c01445f55
Jens Nazarenus <jens@0x6a.de>
2014-01-22 00:15:41 +0100

    better sftp implementation withou errors on mkdir

--- lib/travis-custom-deploy/transfer/sftp.rb
+++ lib/travis-custom-deploy/transfer/sftp.rb
@@ -16,16 +16,46 @@ module TravisCustomDeploy
       def transfer
         Net::SFTP.start(@options['host'], @options['username'], 
                         :password => @options['password']) do |sftp|
+
           for e in @files
-            sftp.upload!(e, "#{@remotedir}/")
+            if File.directory?(e)
+              send_dir(sftp, e, @remotedir)
+            end
           end
         end
       end
 
       def check_options(options)
         raise ArgumentError, 'host name must not be nil' if options['host'].nil?
-        raise ArgumentError, 'username must not be nil' if options['username'].nil?
+        raise ArgumentError, 'userna)e must not be nil' if options['username'].nil?
         raise ArgumentError, 'password must not be nil' if options['password'].nil?
+      end
+
+
+      # Sends a directory to the remote server with the given parameters
+      #
+      # sftp - the SFTP connection
+      # dir - the local dir to transfer
+      # the remote folder where to place the files
+      def send_dir(sftp, dir, remote)
+        Dir.foreach(dir) do |file_name|
+
+          next if file_name =~ /^(\.|\.\.)$/
+
+          localfile = File.join(dir, file_name)
+          if File.directory?(localfile)
+
+            begin 
+              sftp.mkdir!(remote + "/" + file_name)
+            rescue Net::SFTP::StatusException
+              # the directory probably already exists
+            end
+
+            send_dir(sftp, File.join(dir, file_name), File.join(remote, file_name))
+          else
+            sftp.upload!(File.join(dir, file_name), File.join(remote, file_name))
+          end
+        end
       end
 
       # Prepares the remote directory (remote trailing slash)
@@ -34,7 +64,7 @@ module TravisCustomDeploy
         @remotedir = "" if @remotedir.nil?
         @remotedir = @remotedir[0..-2] if @remotedir[-1] == "/"
       end
-      
+
     end
   end
 end
