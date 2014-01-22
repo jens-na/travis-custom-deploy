ad6f4fdd116af2099cea8dd00039deec962e71a2
Jens Nazarenus <jens@0x6a.de>
2014-01-22 20:18:45 +0100

    fix sftp file upload

--- lib/travis-custom-deploy/transfer/sftp.rb
+++ lib/travis-custom-deploy/transfer/sftp.rb
@@ -18,11 +18,14 @@ module TravisCustomDeploy
           for e in @files
             if File.directory?(e)
               send_dir(sftp, e, @remotedir)
+            else
+              sftp.upload!(e, @remotedir + "/" + File.basename(e))
             end
           end
         end
       end
 
+      # Check if the passed arguments are sufficient
       def check_options(options)
         raise ArgumentError, 'host name must not be nil' if options['host'].nil?
         raise ArgumentError, 'userna)e must not be nil' if options['username'].nil?
@@ -36,18 +39,15 @@ module TravisCustomDeploy
       # the remote folder where to place the files
       def send_dir(sftp, dir, remote)
         Dir.foreach(dir) do |file_name|
-
           next if file_name =~ /^(\.|\.\.)$/
 
           localfile = File.join(dir, file_name)
           if File.directory?(localfile)
-
             begin 
               sftp.mkdir!(remote + "/" + file_name)
             rescue Net::SFTP::StatusException
               # the directory probably already exists
             end
-
             send_dir(sftp, File.join(dir, file_name), File.join(remote, file_name))
           else
             sftp.upload!(File.join(dir, file_name), File.join(remote, file_name))
